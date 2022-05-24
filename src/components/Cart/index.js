9ad1e51cb736e4d05193be4e9d0f2f6dc9b7c45e
Lucas Costa e Silva Almeida <lucas.almeida@compasso.com.br>
2022-05-24 11:00:48 -0300

    Estudando a biblioteca "reselect"

---
+++ src/components/Cart/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { connect } from "react-redux";
+import { createSelector } from 'reselect'
+
+function Cart({cart, total, addProduct, setShippingValue}){
+  return(
+    <div>
+      <h1>Carrinho</h1>
+      <p>
+        Items: <strong>{cart.items.length}</strong>
+      </p>
+      <p>
+        Frete: <strong>{cart.shipping_value}</strong>
+      </p>
+      <p>
+        Total: <strong>{total}</strong>
+      </p>
+      <button onClick={()=>{addProduct()}}>Adicionar produto</button>
+      <button onClick={()=>{setShippingValue()}}>Calcular Frete</button>
+    </div>
+  )
+}
+
+const calculateTotal = createSelector(
+  state => state.items,
+  state => state.shipping_value,
+  (items, shipping_value) => {
+    return  items.reduce((subtotal, item)=> subtotal + item.price, 0) + shipping_value
+  }
+)
+const mapStateToProps = state =>({
+  cart: state,
+  total: calculateTotal(state.items)
+});
+
+const mapDispatchToProps = dispatch =>({
+  addProduct: () => dispatch({type: 'ADD'}),
+  setShippingValue: () => dispatch({type: 'SET_SHIPPING'})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore } from "redux";
+
+import cart from "./cart";
+
+const store = createStore(
+  cart
+);
+
+export default store;
